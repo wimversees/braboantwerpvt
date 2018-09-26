8b5e813c54978422f07a6b7b2441fdfc38c1ade3
Wim Versées <wim@wiver.be>
2018-09-26 10:17:48 +0200

    replacement of gulp and design folder

--- header.php
+++ header.php
@@ -10,19 +10,19 @@
 		<meta name="description" content="<?php page_description(); ?>">
 		<meta name="author" content="WIVER Webdesign & Display Advertising - www.wiver.be">
 		
-		<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_stylesheet_directory_uri(); ?>/design/img/icons/favicon/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_stylesheet_directory_uri(); ?>/design/img/icons/favicon/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_stylesheet_directory_uri(); ?>/design/img/icons/favicon/favicon-16x16.png">
-        <link rel="manifest" href="<?php echo get_stylesheet_directory_uri(); ?>/design/img/icons/favicon/site.webmanifest">
-        <link rel="mask-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/design/img/icons/favicon/safari-pinned-tab.svg" color="#000000">
-        <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/design/img/icons/favicon/favicon.ico">
+		<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_stylesheet_directory_uri(); ?>/design/img/favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_stylesheet_directory_uri(); ?>/design/img/favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_stylesheet_directory_uri(); ?>/design/img/favicon/favicon-16x16.png">
+        <link rel="manifest" href="<?php echo get_stylesheet_directory_uri(); ?>/design/img/favicon/site.webmanifest">
+        <link rel="mask-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/design/img/favicon/safari-pinned-tab.svg" color="#000000">
+        <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/design/img/favicon/favicon.ico">
         <meta name="msapplication-TileColor" content="#ffffff">
-        <meta name="msapplication-config" content="<?php echo get_stylesheet_directory_uri(); ?>/design/img/icons/favicon/browserconfig.xml">
+        <meta name="msapplication-config" content="<?php echo get_stylesheet_directory_uri(); ?>/design/img/favicon/browserconfig.xml">
 		<meta name="theme-color" content="#ffffff">
 		
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 
-		<link href="<?php echo get_stylesheet_directory_uri(); ?>/design/css/khc-dragons.css" rel="stylesheet">
+		<link href="<?php echo get_stylesheet_directory_uri(); ?>/design/css/main.css" rel="stylesheet">
 
 		<?php include 'parts/header-structured-data-info.php'; ?>
 
