a92e83de3646659bdc0d99ae5211cbd1ca32b733
Wim Versées <wim@wiver.be>
2018-10-23 19:05:34 +0100

    clear up config

--- data/config.php
+++ data/config.php
@@ -7,15 +7,7 @@ function getConfig(){
 		'version'								=> "0.9.1",
 		'hashkey'								=> "ripemd160",
 		
-		'default-og-image'			=> '/img/default-og-image.jpg',
-		'default-og-description'	=> 'ETF Congress Barcelona 2018',
-		'original_file_extension'	=> '_original',
-		'allowed_file_extensions'	=> '.doc,.docx,.pdf',
-		'allowed_media_extensions'	=> '.jpg,.jpeg',
-		'final_file_extension'		=> '_final',
-		'file_languages'			=> 'en|fr|de|es|it',
-		'mail_name'					=> 'ETF Congress 2017',
-		'mail_send_address'			=> 'noreply@etf-2017congress.org',
+		'default-og-image'						=> '/img/default-og-image.jpg',
 	);
 }
 
