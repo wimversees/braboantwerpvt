a06e678525872ea1e5411c7c3e74b1c77693af2d
Wim Versées <wim@wiver.be>
2018-11-16 16:28:51 +0100

    show svg images in wp backend

--- parts/functions/functions-media.php
+++ parts/functions/functions-media.php
@@ -11,3 +11,23 @@ function cc_mime_types($mimes) {
 	return $mimes;
 }
 add_filter('upload_mimes', 'cc_mime_types');
+
+/**
+ * This function enhanced the display of svg images in the wordpress backend
+ *
+ * @return void
+ */
+function fix_svg_thumb_display() {
+	echo 
+	'<style>
+		#postimagediv .inside img{
+			width: 100% !important; 
+			height: auto !important;
+		}
+	  	td.media-icon img[src$=".svg"], img[src$=".svg"].attachment-post-thumbnail { 
+			width: 100% !important; 
+			height: auto !important; 
+	  	}
+	</style>';
+}
+add_action('admin_head', 'fix_svg_thumb_display');
