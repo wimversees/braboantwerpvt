4bf114eae70efba75033b62f907ff5f332b88d23
Wim Versées <wim@wiver.be>
2019-04-04 12:20:35 +0200

    devimg random color

--- parts/functions/functions-development.php
+++ parts/functions/functions-development.php
@@ -14,11 +14,25 @@
  */
 function devimage(int $width = 100, int $height = 100, string $text = '', bool $echo = true){
 	if(strlen($text) > 0){
-		$text = $text . ' ' . $width . 'x' . $height;
+		$text = $text . ' - ' . $width . 'x' . $height;
 	}
-	$url = '//placehold.it/' . $width . 'x' . $height . '/efefef/ddd?text=' . $text;
+	$url = '//placehold.it/' . $width . 'x' . $height . '/' . randomHexColor() . '/ddd?text=' . $text;
 	if($echo) {	
 		echo $url; return;
 	}
 	return $url;
+}
+
+/**
+ * This function creates a random hex color part (two digits)
+ */
+function randomHexColorPart() {
+    return str_pad( dechex( mt_rand( 0, 255 ) ), 2, '0', STR_PAD_LEFT);
+}
+
+/**
+ * This function creates a random hex color
+ */
+function randomHexColor() {
+    return randomHexColorPart() . randomHexColorPart() . randomHexColorPart();
 }
