4bf25fdac49de138812600fbc35fb1da3a21b244
Wim Versées <wim@wiver.be>
2020-04-27 13:45:19 +0200

    text example for bootstrap components

--- parts/functions/all-bootstrap-components.php
+++ parts/functions/all-bootstrap-components.php
@@ -7,6 +7,13 @@
             <h4>H4 Bootstrap Components</h4>
             <h5>H5 Bootstrap Components</h5>
             <h6>H6 Bootstrap Components</h6>
+            <p>Lorem Ipsum is simply dummy <strong>text of the printing and typesetting industry</strong>. Lorem Ipsum has been the industry's
+                standard dummy text ever
+                since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only
+                five centuries, but also the leap <u>into electronic typesetting, remaining essentially unchanged. It was popularised in the
+                    1960s with</u>
+                the release of Letraset sheets containing <i>Lorem Ipsum passages, and more recently with desktop publishing</i> software like Aldus
+                PageMaker including versions of Lorem Ipsum.</p>
 
             <hr>
 
--- parts/functions/functions-development.php
+++ parts/functions/functions-development.php
@@ -1,8 +1,8 @@
 <?php
 
 /*
-* SECTION DEVELOPMENT
-*/
+ * SECTION DEVELOPMENT
+ */
 
 /**
  * This function returns a palceholder image for a given width, height and possible description
@@ -12,34 +12,58 @@
  * @param string $text
  * @return void
  */
-function devimage(int $width = 100, int $height = 100, string $text = '', bool $echo = true){
-	if(strlen($text) > 0){
-		$text = $text . ' - ' . $width . 'x' . $height;
-	}
-	$url = '//placehold.it/' . $width . 'x' . $height . '/' . randomHexColor() . '/ddd?text=' . $text;
-	if($echo) {	
-		echo $url; return;
-	}
-	return $url;
+function devimage(int $width = 100, int $height = 100, string $text = '', bool $echo = true)
+{
+    if (strlen($text) > 0) {
+        $text = $text . ' - ' . $width . 'x' . $height;
+    }
+    $url = '//placehold.it/' . $width . 'x' . $height . '/' . randomHexColor() . '/ddd?text=' . $text;
+    if ($echo) {
+        echo $url;return;
+    }
+    return $url;
+}
+
+/**
+ * This function returns a palceholder image for a given width, height and possible description
+ *
+ * @param integer $width
+ * @param integer $height
+ * @param string $text
+ * @return void
+ */
+function devtext(int $length = 500, string $textStart = '', bool $echo = true)
+{
+    if (strlen($text) > 0) {
+        $text = $text . ' - ' . $width . 'x' . $height;
+    }
+    $url = '//placehold.it/' . $width . 'x' . $height . '/' . randomHexColor() . '/ddd?text=' . $text;
+    if ($echo) {
+        echo $url;return;
+    }
+    return $url;
 }
 
 /**
  * This function creates a random hex color part (two digits)
  */
-function randomHexColorPart() {
-    return str_pad( dechex( mt_rand( 0, 255 ) ), 2, '0', STR_PAD_LEFT);
+function randomHexColorPart()
+{
+    return str_pad(dechex(mt_rand(0, 255)), 2, '0', STR_PAD_LEFT);
 }
 
 /**
  * This function creates a random hex color
  */
-function randomHexColor() {
+function randomHexColor()
+{
     return randomHexColorPart() . randomHexColorPart() . randomHexColorPart();
 }
 
 /**
  * This function checks if the environments is PRO, based on localhost base url.
  */
-function IsProEnvironment() {
-    return !(strpos('localhost:81', strtolower($_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'])) !== false);	
+function IsProEnvironment()
+{
+    return !(strpos('localhost:81', strtolower($_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'])) !== false);
 }
