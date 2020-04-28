13c4988581807c45024fdaf4c64f48843288f52c
Wim Versées <wim@wiver.be>
2020-04-28 21:16:25 +0200

    move media functions

--- functions.php
+++ functions.php
@@ -1,45 +1,40 @@
-<?php 
+<?php
 
 // include custom function parts
-require_once('parts/functions/functions-general.php');
-require_once('parts/functions/functions-posts.php');
-require_once('parts/functions/functions-post-actions.php');
-require_once('parts/functions/functions-og-tags.php');
-require_once('parts/functions/functions-caching.php');
-require_once('parts/functions/functions-media.php');
+require_once 'parts/functions/functions-general.php';
+require_once 'parts/functions/functions-posts.php';
+require_once 'parts/functions/functions-post-actions.php';
+require_once 'parts/functions/functions-og-tags.php';
+require_once 'parts/functions/functions-caching.php';
+require_once 'parts/functions/functions-media.php';
 // require_once('parts/functions/functions-gallery.php');
-require_once('parts/functions/functions-breadcrumb.php');
-require_once('parts/functions/functions-development.php');
-require_once('parts/functions/functions-sitemap.php');
+require_once 'parts/functions/functions-breadcrumb.php';
+require_once 'parts/functions/functions-development.php';
+require_once 'parts/functions/functions-sitemap.php';
 
 // theme supports
-add_theme_support( 'menus' );
-add_theme_support( 'post-thumbnails' );
+add_theme_support('menus');
+add_theme_support('post-thumbnails');
 
 // Register Custom Navigation Walker
-require_once('parts/wiver_navwalker.php');
-
-// Register custom image sizes
-add_filter('jpeg_quality', function($arg){return 85;});
-add_filter('wp_editor_set_quality', function($arg){return 85;} );
-add_image_size('gallery-thumbnail', 350, 250, true);
-add_image_size('gallery-full', 1400, 950, false);
-// add_image_size('block-image', 255, 165, true);
+require_once 'parts/wiver_navwalker.php';
 
 // add featured image to custom post types
 // add_post_type_support( 'custom-type', 'thumbnail' );
 
 // remove contact form 7 wpcf7 from admin menu for not admins
 if (!(current_user_can('administrator'))) {
-	function remove_wpcf7() {
-	    remove_menu_page( 'wpcf7' ); 
-	}
-	add_action('admin_menu', 'remove_wpcf7');
+    function remove_wpcf7()
+    {
+        remove_menu_page('wpcf7');
+    }
+    add_action('admin_menu', 'remove_wpcf7');
 }
 
 // global action settings
-function global_settings() {  
-    // Add category metabox to page 
-	// register_taxonomy_for_object_type('category', 'page'); 
+function global_settings()
+{
+    // Add category metabox to page
+    // register_taxonomy_for_object_type('category', 'page');
 }
-add_action( 'init', 'global_settings' );
+add_action('init', 'global_settings');
--- parts/functions/functions-media.php
+++ parts/functions/functions-media.php
@@ -1,4 +1,11 @@
-<?php 
+<?php
+
+// Register custom image sizes
+add_filter('jpeg_quality', function ($arg) {return 85;});
+add_filter('wp_editor_set_quality', function ($arg) {return 85;});
+add_image_size('gallery-thumbnail', 350, 250, true);
+add_image_size('gallery-full', 1400, 950, false);
+// add_image_size('block-image', 255, 165, true);
 
 /**
  * This function enables svg as possible uploadable images
@@ -6,10 +13,11 @@
  * @param [type] $mimes
  * @return void
  */
-function cc_mime_types($mimes) {
-	$mimes['svg'] = 'image/svg+xml';
-	$mimes['webp'] = 'image/webp';
-	return $mimes;
+function cc_mime_types($mimes)
+{
+    $mimes['svg']  = 'image/svg+xml';
+    $mimes['webp'] = 'image/webp';
+    return $mimes;
 }
 add_filter('upload_mimes', 'cc_mime_types');
 
@@ -18,16 +26,17 @@ add_filter('upload_mimes', 'cc_mime_types');
  *
  * @return void
  */
-function fix_svg_thumb_display() {
-	echo 
-	'<style>
+function fix_svg_thumb_display()
+{
+    echo
+        '<style>
 		#postimagediv .inside img{
-			width: 100% !important; 
+			width: 100% !important;
 			height: auto !important;
 		}
-	  	td.media-icon img[src$=\'.svg\'], img[src$=\'.svg\'].attachment-post-thumbnail { 
-			width: 100% !important; 
-			height: auto !important; 
+	  	td.media-icon img[src$=\'.svg\'], img[src$=\'.svg\'].attachment-post-thumbnail {
+			width: 100% !important;
+			height: auto !important;
 	  	}
 	</style>';
 }
