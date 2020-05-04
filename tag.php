a712934d52fe664cab033e68023fc52dc0acc2e6
Wim Versées <wim@wiver.be>
2020-05-04 14:04:13 +0200

    tag page

--- tag.php
+++ tag.php
@@ -1,11 +1,23 @@
 <?php get_header();?>
 
-<?php while ( have_posts() ) : the_post(); ?>
-
-<?php the_content(); ?>
-
-<?php endwhile; ?>
-
-<?php get_sidebar();?>
+<div class="container">
+    <div class="row">
+        <div class="col-md-9">
+                <h1>Tag: <?php echo single_tag_title(); ?></h1>
+                <?php 
+                // all posts
+                while ( have_posts() ) : the_post(); ?>
+                    <div class="col-6 col-md-4">
+                        <?php the_title(); ?>
+                    </div>
+                <?php 
+                endwhile; 
+                wp_reset_postdata();?>
+        </div>
+        <div class="col-md-3">
+            <?php get_sidebar();?> 
+        </div>
+    </div>
+</div>
 
 <?php get_footer();?>
