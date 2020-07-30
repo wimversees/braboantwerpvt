fe78b99bdfb8410363faa711bcdb850dc52f7d7f
Wim Versées <wim@wiver.be>
2020-07-30 10:36:21 +0200

    language switcher with flags

--- parts/languages/language-switcher.php
+++ parts/languages/language-switcher.php
@@ -3,13 +3,19 @@ if (function_exists('icl_get_languages')) {
     $languages = icl_get_languages('skip_missing=1');
     if (1 < count($languages)) {
         ?>
-<ul class="lang-switcher">
+<ul class="lang-switcher navbar-nav">
     <?php
 foreach ($languages as $l) {
             if (!$l['active']) { ?>
     <li class="nav-item"><a class="nav-link" href="<?php echo $l['url']; ?>" title="<?php t('wpml-check-in');
-                echo $l['translated_name']; ?>"><?php echo c('multilanguage-flags-enabled') ? getFrontEndFile('parts/languages/flags/' . $l['code'] . '.svg') : '';
-                echo $l['code']; ?></a></li>
+                echo $l['translated_name']; ?>">
+            <?php if (c('multilanguage-flags-enabled')) { ?>
+            <img src="<?php echo getFrontEndFile('/parts/languages/flags/' . $l['code'] . '.svg'); ?>" alt="<?php t('wpml-check-in');
+                    echo $l['translated_name']; ?>" />
+            <?php }
+                echo $l['code'];
+                ?></a>
+    </li>
     <?php }
         } ?>
 </ul>
