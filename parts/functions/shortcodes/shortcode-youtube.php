871802326b2787d13663bac41eb94aa2a7c37fc0
Wim Versées <wim@wiver.be>
2020-09-18 12:20:28 +0200

    shortcode documentation

--- parts/functions/shortcodes/shortcode-youtube.php
+++ parts/functions/shortcodes/shortcode-youtube.php
@@ -9,10 +9,17 @@ function wiver_shortcode_youtube($atts)
 {
     // merge parameters with default set
     $a = shortcode_atts(array(
+        'doc'  => '',
         'id'   => '',
         'name' => 'Video of ' . get_bloginfo('name'),
     ), $atts);
 
+    // return documentation table
+    if (strlen($a['doc']) > 0) {
+        return wiver_shortcode_youtube_documentation();
+    }
+
+    // create output html
     $linkHtml = '<iframe ';
     $linkHtml .= 'title="' . $a['name'] . '" ';
     $linkHtml .= 'data-src="https://www.youtube.com/embed/' . $a['id'] . '?ecver=1&amp;iv_load_policy=3&amp;rel=0&amp;showinfo=0&amp;yt:stretch=16:9&amp;autohide=1&amp;color=red&amp;width=560&amp;width=560" ';
@@ -23,3 +30,25 @@ function wiver_shortcode_youtube($atts)
     return $linkHtml;
 }
 add_shortcode('youtube', 'wiver_shortcode_youtube');
+
+function wiver_shortcode_youtube_documentation()
+{
+    $documentation = [
+        "shortcode"  => "youtube",
+        "attributes" => [
+            [
+                "attr"    => "id",
+                "default" => "",
+                "comment" => "Youtube Video ID",
+                "example" => "Lwk8e2q4qHo",
+            ],
+            [
+                "attr"    => "name",
+                "default" => "Title of the current post",
+                "comment" => "This parameter will be the title attribute of the iframe",
+                "example" => "The title of the iframe",
+            ],
+        ],
+    ];
+    return shortcode_documentation($documentation);
+}
