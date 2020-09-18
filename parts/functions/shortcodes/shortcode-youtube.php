14a75d1bd4335cb6231e48ff3aa7bccc1eb4a847
Wim Versées <wim@wiver.be>
2020-09-18 15:15:21 +0200

    youtube shortcode doc and fallback toward title

--- parts/functions/shortcodes/shortcode-youtube.php
+++ parts/functions/shortcodes/shortcode-youtube.php
@@ -9,9 +9,12 @@ function wiver_shortcode_youtube($atts)
 {
     // merge parameters with default set
     $a = shortcode_atts(array(
-        'doc'  => '',
-        'id'   => '',
-        'name' => 'Video of ' . get_bloginfo('name'),
+        'doc'     => '',
+        'id'      => '',
+        'name'    => '',
+        'title'   => get_the_title(get_the_ID()),
+        'cc'      => '0',
+        'cc_lang' => get_og_locale(),
     ), $atts);
 
     // return documentation table
@@ -19,10 +22,15 @@ function wiver_shortcode_youtube($atts)
         return wiver_shortcode_youtube_documentation();
     }
 
+    // fallback from name to title
+    if (strlen($a['name']) > 0) {
+        $a['title'] = $a['name'];
+    }
+
     // create output html
     $linkHtml = '<iframe ';
-    $linkHtml .= 'title="' . $a['name'] . '" ';
-    $linkHtml .= 'data-src="https://www.youtube.com/embed/' . $a['id'] . '?ecver=1&amp;iv_load_policy=3&amp;rel=0&amp;showinfo=0&amp;yt:stretch=16:9&amp;autohide=1&amp;color=red&amp;width=560&amp;width=560" ';
+    $linkHtml .= 'title="' . $a['title'] . '" ';
+    $linkHtml .= 'data-src="https://www.youtube.com/embed/' . $a['id'] . '?ecver=1&amp;iv_load_policy=3&amp;rel=0&amp;showinfo=0&amp;yt:stretch=16:9&amp;autohide=1&amp;color=red&amp;width=560' . ($a['cc'] == '1' ? '&amp;cc_lang_pref=' . $a['cc_lang'] . '&amp;cc_load_policy=1' : '') . '" ';
     $linkHtml .= 'class="youtube" ';
     $linkHtml .= 'width="560" height="315" allowtransparency="true" frameborder="0"';
     $linkHtml .= '>';
@@ -44,9 +52,27 @@ function wiver_shortcode_youtube_documentation()
             ],
             [
                 "attr"    => "name",
-                "default" => "Title of the current post",
+                "default" => "",
                 "comment" => "This parameter will be the title attribute of the iframe",
                 "example" => "The title of the iframe",
+            ],
+            [
+                "attr"    => "title",
+                "default" => "Title of the current post, or name parameter when populated",
+                "comment" => "This parameter will be the title attribute of the iframe",
+                "example" => "The title of the iframe",
+            ],
+            [
+                "attr"    => "cc",
+                "default" => "0",
+                "comment" => "Enables the captions of the video",
+                "example" => "1",
+            ],
+            [
+                "attr"    => "cc_lang",
+                "default" => "The current locale.",
+                "comment" => "Sets the language of the captions",
+                "example" => "en",
             ],
         ],
     ];
